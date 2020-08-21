3b88c9398de07ddc8b89294b2e3a6d8429be4312
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-08-20 20:54:12 -0400

    Don't reauth on every request.

--- src/cmd/client.go
+++ src/cmd/client.go
@@ -8,31 +8,44 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var numRequests int
+var token string
+
 func sendRequest(request *http.Request, username, password string) (*http.Response, error) {
 
-	requestBody, err := json.Marshal(map[string]string{
-		"username": username,
-		"password": password,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// authenticate
-	resp, err := http.Post("https://hub.docker.com/v2/users/login/", "application/json", bytes.NewBuffer(requestBody))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	var result map[string]string
-	var token string
-
-	json.NewDecoder(resp.Body).Decode(&result)
-
-	token = result["token"]
-
 	client := &http.Client{}
+
+	// If username and password are blank, skip all of these, we're not doing an authenticated call.
+	if username == "" && password == "" {
+		return client.Do(request)
+	}
+
+	// The Docker Hub API isn't well documented. It's not clear for how long can
+	// an auth token be used. For now, it's assumed that it will be good for every 6,000 requests.
+	if numRequests%7000 == 0 {
+
+		requestBody, err := json.Marshal(map[string]string{
+			"username": username,
+			"password": password,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// authenticate
+		resp, err := http.Post("https://hub.docker.com/v2/users/login/", "application/json", bytes.NewBuffer(requestBody))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		var result map[string]string
+
+		json.NewDecoder(resp.Body).Decode(&result)
+
+		token = result["token"]
+		numRequests++
+	}
 
 	request.Header.Set("Authorization", "JWT "+token)
 
