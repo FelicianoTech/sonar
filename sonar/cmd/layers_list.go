8421c00dbb2f0dd9a74c8a412512925b71740f1a
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-30 23:54:34 -0500

    Improve layers list command.

--- sonar/cmd/layers_list.go
+++ sonar/cmd/layers_list.go
@@ -11,56 +11,59 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var (
-	//sumSizeFl bool
-
-	layersListCmd = &cobra.Command{
-		Use:   "list <image-name> <tag>",
-		Short: "Displays the layers for a given Docker image",
-		Long: `The output of instruction is limited to 55 characters.
+var layersListCmd = &cobra.Command{
+	Use:   "list <image>",
+	Short: "Displays the layers for a given Docker image",
+	Long: `The output of instruction is limited to 55 characters.
 `,
-		Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 
-			if len(args) == 0 {
-				log.Fatal("Please provide a Docker image.")
-				os.Exit(1)
+		if len(args) == 0 {
+			log.Fatal("Please provide a Docker image.")
+			os.Exit(1)
+		}
+
+		image, err := docker.ParseImageRef(args[0])
+		if err != nil {
+			fmt.Errorf("%s, err")
+		}
+
+		image.ShowTag = false
+
+		dockerLayers, err := docker.GetAllLayers(image.String(), image.Tag)
+		if err != nil {
+			log.Errorf("Failed getting layers for Docker tag: %s", err)
+		}
+
+		fmt.Println("The layers are: ")
+
+		var counter int
+
+		for _, layer := range dockerLayers {
+
+			var digestStr string
+			var sizeStr string
+
+			counter++
+
+			if layer.Digest == "" {
+				digestStr = "<not-assigned>\t\t\t\t\t\t\t"
+			} else {
+				digestStr = layer.Digest
 			}
 
-			dockerLayers, err := docker.GetAllLayers(args[0], args[1])
-			if err == nil {
-				log.Errorf("Failed getting layers for Docker tag: %s", err)
+			if layer.Size == 0 {
+				sizeStr = "0 B\t"
+			} else {
+				sizeStr = ByteCountBinary(layer.Size)
 			}
 
-			fmt.Println("The layers are: ")
+			fmt.Printf("%d:\t%s\t%s\t%.55s\n", counter, digestStr, sizeStr, layer.Instruction)
+		}
 
-			var counter int
-
-			for _, layer := range dockerLayers {
-
-				var digestStr string
-				var sizeStr string
-
-				counter++
-
-				if layer.Digest == "" {
-					digestStr = "<not-assigned>\t\t\t\t\t\t\t"
-				} else {
-					digestStr = layer.Digest
-				}
-
-				if layer.Size == 0 {
-					sizeStr = "0 B\t"
-				} else {
-					sizeStr = ByteCountBinary(layer.Size)
-				}
-
-				fmt.Printf("%d:\t%s\t%s\t%.55s\n", counter, digestStr, sizeStr, layer.Instruction)
-			}
-
-			fmt.Printf("Total layers: %d\n", len(dockerLayers))
-		},
-	}
-)
+		fmt.Printf("Total layers: %d\n", len(dockerLayers))
+	},
+}
 
 func init() {
 	layersCmd.AddCommand(layersListCmd)
